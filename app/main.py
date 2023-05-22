13a30fe387bcde88fcebfb2bd94ef9c5f0f8f3d1
Andres Moschini <andresmoschini@gmail.com>
2023-05-22 19:20:11 -0300

    feat: add router for admin

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import Depends, FastAPI
 
+from app.dependencies import get_query_token
+from app.internal import admin
 from app.routers import items, users, static
 
 app = FastAPI()
@@ -8,6 +10,13 @@ app = FastAPI()
 app.include_router(users.router)
 app.include_router(items.router)
 app.include_router(static.router)
+app.include_router(
+    admin.router,
+    prefix="/admin",
+    tags=["admin"],
+    dependencies=[Depends(get_query_token)],
+    responses={418: {"description": "I'm a teapot"}},
+)
 
 
 @app.get("/")
